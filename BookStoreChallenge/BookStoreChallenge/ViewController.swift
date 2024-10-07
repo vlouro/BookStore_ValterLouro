8f96fb7f7c126c3acc2d91f3f070da02759de093
moongyinterprojects <valterlouro@gmail.com>
2024-10-07 15:29:34 +0100

    Commit 1 - Created and added project

---
+++ BookStoreChallenge/BookStoreChallenge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BookStoreChallenge
+//
+//  Created by Valter Louro on 07/10/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ BookStoreChallenge/BookStoreChallengeTests/BookStoreChallengeTests.swift
@@ -0,0 +1,17 @@
+//
+//  BookStoreChallengeTests.swift
+//  BookStoreChallengeTests
+//
+//  Created by Valter Louro on 07/10/2024.
+//
+
+import Testing
+@testable import BookStoreChallenge
+
+struct BookStoreChallengeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
